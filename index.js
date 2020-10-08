a7c2c5a1b6f9b901e709db01de3eddccf58363d3
axdiaz <axeldiaz@outlook.com>
2020-10-08 11:57:36 -0300

    cambioEquivocado 2

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ console.log("esta linea la puse solo en el branch integration")
 console.log("este cambio es de fix1")
 
 console.log("este es el cambio que no queria hacer en integration, queria hacerlo en fix2")
+console.log("este es el segundo cambio que no queria hacer en integration, queria hacerlo en fix2")
